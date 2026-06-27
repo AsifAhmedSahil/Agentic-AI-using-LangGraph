532abde1d882192a3ff2026de3eaf7dd7b046a24
Asif Ahmed Sahil <asifahmedsahil.12@gmail.com>
2026-06-27 22:57:25 +0600

    chatbot ui with streamlit

--- langgraph-chatbot/chatbot_frontend.py
+++ langgraph-chatbot/chatbot_frontend.py
@@ -1,8 +1,35 @@
 import streamlit as st
+from  chatbot_backend import chatbot
 
-with st.chat_message('user'):
-    st.text("hi")
+from langchain_core.messages import HumanMessage
+
+CONFIG = {"configurable":{"thread_id":"thread-1"}}
+
+if "message_history" not in st.session_state:
+    st.session_state["message_history"] = []
+
+# loading all conversation
+for message in st.session_state["message_history"]:
+    with st.chat_message(message["role"]):
+        st.text(message["content"])
 
 
 
-user_input = st.chat_input("Type_here")
+user_input = st.chat_input("Type Here...")
+
+if user_input:
+    st.session_state["message_history"].append({"role":"user","content":user_input})
+    with st.chat_message("user"):
+        st.text(user_input)
+
+    response = chatbot.invoke({"messages":[HumanMessage(content=user_input)]},config=CONFIG)
+    ai_messages = response["messages"][-1].content
+
+
+    st.session_state["message_history"].append({"role":"assistant","content":ai_messages})
+    with st.chat_message("assistant"):
+        st.text(ai_messages)
+
+
+    
+
